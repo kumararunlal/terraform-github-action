abb6238d982bb7fd066fad7fbbfa48920f4df531
Arun Kumar <a_kumarlal@hotmail.com>
2025-04-22 13:00:43 +0530

    terraform block added

---
+++ src/terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "cicd-rg"
+    storage_account_name = "gitauto"
+    container_name       = "terraformbackend"
+    key                  = "deploy.tfstate"
+    #use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  #use_oidc = true
+}
+
+# Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
+# Create a resource group
+resource "azurerm_resource_group" "terra-git-action" {
+  name     = "terragitaction"
+  location = "Canada Central"
+}
